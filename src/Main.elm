2237462873b30bdbc4fe3cc1a7533e02aed483b4
skht777 <skht@skhtlab.com>
2017-12-25 16:29:22 +0900

    天才なので振ったダイスそれぞれの目と合計を表示するようにできた

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, button, input, span)
+import Html exposing (Html, text, div, h1, h3, button, input, span)
 import Html.Attributes exposing (class, type_, value)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 import Random
 
 
@@ -10,12 +10,16 @@ import Random
 
 
 type alias Model =
-    { dieFace : Int }
+    { dieFaces : List Int, faceNum : Int, times : Int }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model 1, Cmd.none )
+    ( { dieFaces = [ ]
+      , faceNum = 6
+      , times = 1
+      }
+      , Cmd.none )
 
 
 
@@ -24,17 +28,33 @@ init =
 
 type Msg
     = Roll
-    | NewFace Int
+    | NewFaces (List Int)
+    | FaceNum String
+    | Times String
 
+toInt : String -> Int -> Int
+toInt str default = Result.withDefault default 
+                    <| String.toInt str
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg model = 
     case msg of
         Roll ->
-            ( model, Random.generate NewFace (Random.int 1 6) )
+            let
+                cmd = Random.int 1 model.faceNum
+                      |> Random.list model.times
+                      |> Random.generate NewFaces
+            in                    
+            ( model, cmd )
 
-        NewFace newFace ->
-            ( Model newFace, Cmd.none )
+        NewFaces newFaces ->
+            ( { model | dieFaces = newFaces }, Cmd.none )
+
+        Times newTimes ->
+            ( { model | times = (toInt newTimes model.times) }, Cmd.none )
+
+        FaceNum newNum ->
+            ( { model | faceNum = (toInt newNum model.faceNum) }, Cmd.none )
 
 
 
@@ -42,13 +62,22 @@ update msg model =
 
 
 view : Model -> Html Msg
-view model =
+view { dieFaces, faceNum, times } =
+    let
+        sum = if List.isEmpty dieFaces
+              then "-"
+              else List.sum dieFaces |> toString
+        list = List.foldl (\a b -> a ++ ", " ++ b)
+                    ( Maybe.withDefault "-" (List.head dieFaces |> Maybe.map toString) )
+                    ( List.map toString ( Maybe.withDefault [] <| List.tail dieFaces ) )
+    in
     div []
-        [ h1 [] [ text (toString model.dieFace) ]
+        [ h1 [] [ text sum ]
+        , h3 [] [ text list ]
         , div [ class "die-setting" ]
-            [ input [ class "die", type_ "number", value "1" ] []
+            [ input [ class "die", type_ "number", value (toString times), onInput Times ] []
             , span [] [ text "d" ]
-            , input [ class "die", type_ "number", value "6" ] []
+            , input [ class "die", type_ "number", value (toString faceNum), onInput FaceNum ] []
             ]
         , button [ onClick Roll ] [ text "Roll" ]
         ]
